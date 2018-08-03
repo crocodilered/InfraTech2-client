f3f392d8e879318d65f91c16b12c00ea0a7792e9
Serge Zolotukhin <serge.zolotukhin@gmail.com>
2018-08-03 13:27:39 +0300

    Cosmetic changes.

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,26 +1,40 @@
 <template>
   <v-container fluid>
+    <h1>Dashboard</h1>
     <v-layout row wrap>
-      <v-flex xs12>
-        <h1>Dashboard</h1>
-      </v-flex>
-      <v-flex xs12 md3>
+      <v-flex xs12 md2>
+        <h2>Maintenance</h2>
+        <p><router-link to="/maintenance">Proceed</router-link></p>
         <h2>Directories</h2>
-        <p>
-          <b><a href="#"><router-link to="/maintenance">Maintenance</router-link></a></b><br>
-          <b><a href="#"><router-link to="/dir/contracts">Contracts</router-link></a></b><br>
-          <b><a href="#"><router-link to="/dir/equipment">Equipment</router-link></a></b>
-        </p>
+        <ul v-if="directoriesModel">
+          <li><router-link to="/dir/contracts">{{ directoriesModel.contractsCount }} contracts</router-link></li>
+          <li><router-link to="/dir/equipment">{{ directoriesModel.equipmentCount }} equipment units</router-link></li>
+        </ul>
       </v-flex>
       <v-flex
-        xs12 md3
+        xs12 md4
         v-if="budgetModel"
       >
-        <h2>Budget</h2>
-        <p>
-          Current period is {{ budgetModel.beginDate }} — {{ budgetModel.endDate }}.<br>
-          Execution: {{ budgetModel.execution }}$ of {{ budgetModel.value }}$ ({{ budgetExecInPercents }}%).
-        </p>
+        <v-layout row>
+          <v-flex>
+            <v-progress-circular
+              :rotate="-90"
+              :size="100"
+              :width="15"
+              :value="budgetExecInPercents"
+              color="primary"
+            >
+              {{ budgetExecInPercents }}%
+            </v-progress-circular>
+          </v-flex>
+          <v-flex>
+            <h2>
+              Budget<br>
+              <small>{{ budgetModel.beginDate }} — {{ budgetModel.endDate }}</small>
+            </h2>
+            <p>Execution: {{ budgetModel.execution }} of {{ budgetModel.value }}$</p>
+          </v-flex>
+        </v-layout>
       </v-flex>
     </v-layout>
   </v-container>
@@ -32,30 +46,45 @@ import conf from '@/conf'
 
 const data = function () {
   return {
-    budgetModel: null
+    directoriesModel: null,
+    budgetModel: null,
+    budgetExecInPercents: 0
   }
 }
 
 const computed = {
   ...mapState([
     'user'
-  ]),
-  budgetExecInPercents () {
-    if (this.budgetModel) {
-      return 100 * this.budgetModel.execution / this.budgetModel.value
-    }
-  }
+  ])
 }
 
-const created = function () {
+const mounted = function () {
   conf.api.post('/analytics/dashboard', { token: this.$store.state.token })
-    .then(resp => (this.budgetModel = resp.data.budget))
+    .then((resp) => {
+      this.budgetModel = resp.data.budget
+      this.directoriesModel = resp.data.directories
+      setTimeout(() => { this.budgetExecInPercents = 100 * this.budgetModel.execution / this.budgetModel.value }, 100)
+    })
     .catch(err => console.log('Got HTTP error.', err))
 }
 
 export default {
   data,
   computed,
-  created
+  mounted
 }
 </script>
+
+<style scoped>
+  h2 {
+    margin-top: 30px;
+  }
+  ul {
+    margin-left: 0;
+    padding-left: 0;
+  }
+  li {
+    list-style: none;
+    margin: .2rem 0;
+  }
+</style>
